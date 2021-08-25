1aa72c2a391dfc91cdc89264cab8d9f956a06915
Sean Dyon <18398275+sdyon-secureauth@users.noreply.github.com>
2021-08-25 11:49:46 -0700

    Heroku Update

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api');
 
 var app = express();
 
-
+var port = process.env.PORT || 3001;
 
 app.use(cors());
 //app.options('*', cors());
@@ -20,8 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 app.use('/api', apiRouter);
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = app;
