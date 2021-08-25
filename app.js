3f3933cb8458616fb08e444c504fea2f723928a2
Sean Dyon <18398275+sdyon-secureauth@users.noreply.github.com>
2021-08-25 13:01:01 -0700

    Heroku Update

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 // api defined first to not conflict with React below
 app.use('/api', apiRouter);
 
 // React requires path to go to index.html so that it can handle routes
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend/build", "index.html"));
   });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
